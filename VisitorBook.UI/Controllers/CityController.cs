3182d924836556eb7a5fb107b9c830df5c204f8c
Eren Gaygusuz <gaygusuzeren@gmail.com>
2023-10-05 15:57:28 +0300

    Adding Controllers and Action Methods for Endpoints

---
+++ VisitorBook.UI/Controllers/CityController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VisitorBook.UI.Controllers
+{
+    public class CityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddOrEdit(int id = 0)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AddOrEdit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
+    }
+}
