9b339c1eede7ed11ccee24eadb1ef5e279b24d5e
Eren Gaygusuz <gaygusuzeren@gmail.com>
2023-10-15 11:38:29 +0300

    Fixing StateSelectionDropdown Problem

--- VisitorBook.UI/wwwroot/js/visited-state.js
+++ VisitorBook.UI/wwwroot/js/visited-state.js
@@ -1,96 +1,114 @@
 var dataTable
 
 $(document).ready(function () {
-    $.fn.dataTable.moment('DD/MM/YYYY');
+  $.fn.dataTable.moment('DD/MM/YYYY')
 
-    var exportBtnText = document.getElementById("TableExportBtnText").value;
-    var editBtnText = document.getElementById("TableEditBtnText").value;
-    var deleteBtnText = document.getElementById("TableDeleteBtnText").value;
+  var exportBtnText = document.getElementById('TableExportBtnText').value
+  var editBtnText = document.getElementById('TableEditBtnText').value
+  var deleteBtnText = document.getElementById('TableDeleteBtnText').value
 
-    var activeLanguage = document.getElementById("ActiveLanguage").value;
+  var activeLanguage = document.getElementById('ActiveLanguage').value
 
-    var trLanguagePath = "//cdn.datatables.net/plug-ins/1.13.6/i18n/tr.json";
-    var enLanguagePath = "//cdn.datatables.net/plug-ins/1.13.6/i18n/en-GB.json"
+  var trLanguagePath = '//cdn.datatables.net/plug-ins/1.13.6/i18n/tr.json'
+  var enLanguagePath = '//cdn.datatables.net/plug-ins/1.13.6/i18n/en-GB.json'
 
-    var activeLanguagePath = "";
+  var activeLanguagePath = ''
 
-    if (activeLanguage == "tr-TR") {
-        activeLanguagePath = trLanguagePath;
-    }
+  if (activeLanguage == 'tr-TR') {
+    activeLanguagePath = trLanguagePath
+  } else {
+    activeLanguagePath = enLanguagePath
+  }
 
-    else {
-        activeLanguagePath = enLanguagePath;
-    }
-
-    loadDataTable(exportBtnText, editBtnText, deleteBtnText, activeLanguagePath);
+  loadDataTable(exportBtnText, editBtnText, deleteBtnText, activeLanguagePath)
 })
 
-function loadDataTable(exportBtnText, editBtnText, deleteBtnText, activeLanguagePath) {
-    dataTable = $('#tblData').DataTable({
-        dom: 'lfrtBip',
-        buttons: [
-            {
-                extend: 'pdfHtml5',
-                text: exportBtnText,
-                filename: 'VisitorBook-VisitedStates',
-                orientation: 'landscape',
-                pageSize: 'A4',
-                customize: function (doc) {
-                    doc.content[1].table.widths =
-                        Array(doc.content[1].table.body[0].length + 1).join('*').split('');
+function loadDataTable(
+  exportBtnText,
+  editBtnText,
+  deleteBtnText,
+  activeLanguagePath
+) {
+  dataTable = $('#tblData').DataTable({
+    dom: 'lfrtBip',
+    buttons: [
+      {
+        extend: 'pdfHtml5',
+        text: exportBtnText,
+        filename: 'VisitorBook-VisitedStates',
+        orientation: 'landscape',
+        pageSize: 'A4',
+        customize: function (doc) {
+          doc.content[1].table.widths = Array(
+            doc.content[1].table.body[0].length + 1
+          )
+            .join('*')
+            .split('')
 
-                    doc.content.splice(0, 1);
-                    doc.pageMargins = [20, 20, 20, 20];
-                    doc.defaultStyle.fontSize = 12;
-                    doc.styles.tableHeader.fontSize = 14;
+          doc.content.splice(0, 1)
+          doc.pageMargins = [20, 20, 20, 20]
+          doc.defaultStyle.fontSize = 12
+          doc.styles.tableHeader.fontSize = 14
 
-                    var objLayout = {};
-                    objLayout['hLineWidth'] = function (i) { return .5; };
-                    objLayout['vLineWidth'] = function (i) { return .5; };
-                    objLayout['hLineColor'] = function (i) { return '#aaa'; };
-                    objLayout['vLineColor'] = function (i) { return '#aaa'; };
-                    objLayout['paddingLeft'] = function (i) { return 4; };
-                    objLayout['paddingRight'] = function (i) { return 4; };
-                    doc.content[0].layout = objLayout;
-                },
-                exportOptions: {
-                    columns: [0, 1, 2, 3]
-                }
-            }
-        ],
-        language: {
-            url: activeLanguagePath,
+          var objLayout = {}
+          objLayout['hLineWidth'] = function (i) {
+            return 0.5
+          }
+          objLayout['vLineWidth'] = function (i) {
+            return 0.5
+          }
+          objLayout['hLineColor'] = function (i) {
+            return '#aaa'
+          }
+          objLayout['vLineColor'] = function (i) {
+            return '#aaa'
+          }
+          objLayout['paddingLeft'] = function (i) {
+            return 4
+          }
+          objLayout['paddingRight'] = function (i) {
+            return 4
+          }
+          doc.content[0].layout = objLayout
         },
-        initComplete: function () {
-            var btns = $('.dt-button, .buttons-pdf, .buttons-html5');
-            btns.removeClass();
-            btns.addClass('btn btn-success');
-            btns.attr("id", "ExportBtn");
-
-            $("#ExportBtn").prependTo($("#outside"));
+        exportOptions: {
+          columns: [0, 1, 2, 3],
         },
-        ajax: { url: '/visitedstate/getall' },
-        columns: [
-            {
-                data: 'visitor',
-                width: '30%',
-                render: function (data) {
-                    return data.name + ' ' + data.surname
-                },
-            },
-            { data: 'state.name', width: '15%' },
-            { data: 'state.city.name', width: '15%' },
-            {
-                data: 'date',
-                width: '20%',
-                render: function (data) {
-                    return moment(data).format('DD/MM/YYYY')
-                },
-            },
-            {
-                data: 'id',
-                render: function (data) {
-                    return `
+      },
+    ],
+    language: {
+      url: activeLanguagePath,
+    },
+    initComplete: function () {
+      var btns = $('.dt-button, .buttons-pdf, .buttons-html5')
+      btns.removeClass()
+      btns.addClass('btn btn-success')
+      btns.attr('id', 'ExportBtn')
+
+      $('#ExportBtn').prependTo($('#outside'))
+    },
+    ajax: { url: '/visitedstate/getall' },
+    columns: [
+      {
+        data: 'visitor',
+        width: '30%',
+        render: function (data) {
+          return data.name + ' ' + data.surname
+        },
+      },
+      { data: 'state.name', width: '15%' },
+      { data: 'state.city.name', width: '15%' },
+      {
+        data: 'date',
+        width: '20%',
+        render: function (data) {
+          return moment(data).format('DD/MM/YYYY')
+        },
+      },
+      {
+        data: 'id',
+        render: function (data) {
+          return `
                             <div class="d-flex justify-content-around align-items-center">
                                <a onclick="showInPopup('/visitedstate/addoredit/${data}', 
                                'Update Visited State')" class="btn btn-warning"> ${editBtnText}</a>
@@ -99,120 +117,130 @@ function loadDataTable(exportBtnText, editBtnText, deleteBtnText, activeLanguage
                                </a>
                             </div>
                             `
-                },
-                width: '20%',
-            }
-        ],
-        columnDefs: [{
-            'targets': [4],
-            'orderable': false
-        }]
-    });
+        },
+        width: '20%',
+      },
+    ],
+    columnDefs: [
+      {
+        targets: [4],
+        orderable: false,
+      },
+    ],
+  })
 }
 
 showInPopup = (url, title) => {
-    $.ajax({
-        type: 'GET',
-        url: url,
-        success: function (res) {
-            $('#form-modal .modal-body').html(res)
-            $('#form-modal .modal-title').html(title)
-            $('#form-modal').modal('show')
+  $.ajax({
+    type: 'GET',
+    url: url,
+    success: function (res) {
+      $('#form-modal .modal-body').html(res)
+      $('#form-modal .modal-title').html(title)
+      $('#form-modal').modal('show')
 
-            if ($('#citylist option:selected').val() == 0) {
-                $('#statelist').hide();
-            }
-            else {
-                $('#statelist').show();
-            }
-        },
-    })
+      if ($('#citylist option:selected').val() == 0) {
+        $('#statelist').hide()
+      } else {
+        $('#statelist').show()
+      }
+    },
+  })
 }
 
 function fillStateList(cityId, selectedIndex) {
-    $.ajax({
-        type: 'GET',
-        url: '/state/getallbycity?cityId=' + cityId,
-        success: function (res) {
+  $.ajax({
+    type: 'GET',
+    url: '/state/getallbycity?cityId=' + cityId,
+    success: function (res) {
+      $('#VisitedState_StateId').empty()
 
-            $('#VisitedState_StateId').slice(1).empty();
+      var stateSelectionText = document.getElementById(
+        'AddOrEditModalStateSelectionText'
+      ).value
 
-            $.each(res.data, function (index, value) {
-                $('#VisitedState_StateId').append(
-                    $('<option value="' + value.id + '">' + value.name + '</option>'))
-            }
-            );
+      $('#VisitedState_StateId').append(
+        $('<option disabled value="0">-' + stateSelectionText + '</option>')
+      )
 
-            $('#VisitedState_StateId option:eq(' + selectedIndex + ')').prop('selected', true);
-        },
-        error: function (err) {
-            console.log(err)
-        }
-    })
+      $.each(res.data, function (index, value) {
+        $('#VisitedState_StateId').append(
+          $('<option value="' + value.id + '">' + value.name + '</option>')
+        )
+      })
+
+      $('#VisitedState_StateId option:eq(' + selectedIndex + ')').prop(
+        'selected',
+        true
+      )
+    },
+    error: function (err) {
+      console.log(err)
+    },
+  })
 }
 
 AddRecord = (form) => {
-    try {
-        $.ajax({
-            type: 'POST',
-            url: form.action,
-            data: new FormData(form),
-            contentType: false,
-            processData: false,
-            success: function (res) {
-                if (res.isValid) {
-                    $('#form-modal .modal-body').html('')
-                    $('#form-modal .modal-title').html('')
-                    $('#form-modal').modal('hide')
+  try {
+    $.ajax({
+      type: 'POST',
+      url: form.action,
+      data: new FormData(form),
+      contentType: false,
+      processData: false,
+      success: function (res) {
+        if (res.isValid) {
+          $('#form-modal .modal-body').html('')
+          $('#form-modal .modal-title').html('')
+          $('#form-modal').modal('hide')
 
-                    dataTable.ajax.reload()
-                } else {
-                    $('#form-modal .modal-body').html(res.html)
-                }
-            },
-            error: function (err) {
-                console.log(err)
-            },
-        })
-    } catch (e) {
-        console.log(e)
-    }
+          dataTable.ajax.reload()
+        } else {
+          $('#form-modal .modal-body').html(res.html)
+        }
+      },
+      error: function (err) {
+        console.log(err)
+      },
+    })
+  } catch (e) {
+    console.log(e)
+  }
 
-    return false
+  return false
 }
 
 function deleteRecord(url) {
-    if (confirm('Are you sure to delete this record?')) {
-        try {
-            $.ajax({
-                type: 'DELETE',
-                url: url,
-                success: function (res) {
-                    alert(res.entityValue + ' Deleted Successfully')
+  if (confirm('Are you sure to delete this record?')) {
+    try {
+      $.ajax({
+        type: 'DELETE',
+        url: url,
+        success: function (res) {
+          alert(res.entityValue + ' Deleted Successfully')
 
-                    dataTable.ajax.reload()
-                },
-                error: function (err) {
-                    alert('An Error Occurred While Deleting')
+          dataTable.ajax.reload()
+        },
+        error: function (err) {
+          alert('An Error Occurred While Deleting')
 
-                    console.log(err)
-                },
-            })
-        } catch (e) { }
-    }
+          console.log(err)
+        },
+      })
+    } catch (e) {}
+  }
 
-    return false
+  return false
 }
 
 function cityChange() {
-    if ($('#citylist option:selected').val() == 0) {
-        $('#statelist').hide();
-    }
-    else {
-        $('#statelist').show();
+  if ($('#citylist option:selected').val() == 0) {
+    $('#statelist').hide()
+  } else {
+    $('#statelist').show()
 
-        var cityId = $('#citylist option:selected').val();
+    var cityId = $('#citylist option:selected').val()
 
-        fillStateList(cityId, 0);
-    }
+    fillStateList(cityId, 0)
+  }
 }
--- VisitorBook.UI/wwwroot/js/visitor.js
+++ VisitorBook.UI/wwwroot/js/visitor.js
@@ -1,90 +1,108 @@
 var dataTable
 
 $(document).ready(function () {
-    $.fn.dataTable.moment('DD/MM/YYYY');
+  $.fn.dataTable.moment('DD/MM/YYYY')
 
-    var exportBtnText = document.getElementById("TableExportBtnText").value;
-    var editBtnText = document.getElementById("TableEditBtnText").value;
-    var deleteBtnText = document.getElementById("TableDeleteBtnText").value;
+  var exportBtnText = document.getElementById('TableExportBtnText').value
+  var editBtnText = document.getElementById('TableEditBtnText').value
+  var deleteBtnText = document.getElementById('TableDeleteBtnText').value
 
-    var activeLanguage = document.getElementById("ActiveLanguage").value;
+  var activeLanguage = document.getElementById('ActiveLanguage').value
 
-    var trLanguagePath = "//cdn.datatables.net/plug-ins/1.13.6/i18n/tr.json";
-    var enLanguagePath = "//cdn.datatables.net/plug-ins/1.13.6/i18n/en-GB.json"
+  var trLanguagePath = '//cdn.datatables.net/plug-ins/1.13.6/i18n/tr.json'
+  var enLanguagePath = '//cdn.datatables.net/plug-ins/1.13.6/i18n/en-GB.json'
 
-    var activeLanguagePath = "";
+  var activeLanguagePath = ''
 
-    if (activeLanguage == "tr-TR") {
-        activeLanguagePath = trLanguagePath;
-    }
+  if (activeLanguage == 'tr-TR') {
+    activeLanguagePath = trLanguagePath
+  } else {
+    activeLanguagePath = enLanguagePath
+  }
 
-    else {
-        activeLanguagePath = enLanguagePath;
-    }
-
-    loadDataTable(exportBtnText, editBtnText, deleteBtnText, activeLanguagePath);
+  loadDataTable(exportBtnText, editBtnText, deleteBtnText, activeLanguagePath)
 })
 
-function loadDataTable(exportBtnText, editBtnText, deleteBtnText, activeLanguagePath) {
-    dataTable = $('#tblData').DataTable({
-        dom: 'lfrtBip',
-        buttons: [
-            {
-                extend: 'pdfHtml5',
-                text: exportBtnText,
-                filename: 'VisitorBook-Visitors',
-                orientation: 'landscape',
-                pageSize: 'A4',
-                customize: function (doc) {
-                    doc.content[1].table.widths =
-                        Array(doc.content[1].table.body[0].length + 1).join('*').split('');
+function loadDataTable(
+  exportBtnText,
+  editBtnText,
+  deleteBtnText,
+  activeLanguagePath
+) {
+  dataTable = $('#tblData').DataTable({
+    dom: 'lfrtBip',
+    buttons: [
+      {
+        extend: 'pdfHtml5',
+        text: exportBtnText,
+        filename: 'VisitorBook-Visitors',
+        orientation: 'landscape',
+        pageSize: 'A4',
+        customize: function (doc) {
+          doc.content[1].table.widths = Array(
+            doc.content[1].table.body[0].length + 1
+          )
+            .join('*')
+            .split('')
 
-                    doc.content.splice(0, 1);
-                    doc.pageMargins = [20, 20, 20, 20];
-                    doc.defaultStyle.fontSize = 12;
-                    doc.styles.tableHeader.fontSize = 14;
+          doc.content.splice(0, 1)
+          doc.pageMargins = [20, 20, 20, 20]
+          doc.defaultStyle.fontSize = 12
+          doc.styles.tableHeader.fontSize = 14
 
-                    var objLayout = {};
-                    objLayout['hLineWidth'] = function (i) { return .5; };
-                    objLayout['vLineWidth'] = function (i) { return .5; };
-                    objLayout['hLineColor'] = function (i) { return '#aaa'; };
-                    objLayout['vLineColor'] = function (i) { return '#aaa'; };
-                    objLayout['paddingLeft'] = function (i) { return 4; };
-                    objLayout['paddingRight'] = function (i) { return 4; };
-                    doc.content[0].layout = objLayout;
-                },
-                exportOptions: {
-                    columns: [0, 1, 2, 3]
-                }
-            }
-        ],
-        language: {
-            url: activeLanguagePath,
+          var objLayout = {}
+          objLayout['hLineWidth'] = function (i) {
+            return 0.5
+          }
+          objLayout['vLineWidth'] = function (i) {
+            return 0.5
+          }
+          objLayout['hLineColor'] = function (i) {
+            return '#aaa'
+          }
+          objLayout['vLineColor'] = function (i) {
+            return '#aaa'
+          }
+          objLayout['paddingLeft'] = function (i) {
+            return 4
+          }
+          objLayout['paddingRight'] = function (i) {
+            return 4
+          }
+          doc.content[0].layout = objLayout
         },
-        initComplete: function () {
-            var btns = $('.dt-button, .buttons-pdf, .buttons-html5');
-            btns.removeClass();
-            btns.addClass('btn btn-success');
-            btns.attr("id", "ExportBtn");
-
-            $("#ExportBtn").prependTo($("#outside"));
+        exportOptions: {
+          columns: [0, 1, 2, 3],
         },
-        ajax: { url: '/visitor/getall' },
-        columns: [
-            { data: 'name', width: '30%' },
-            { data: 'surname', width: '30%' },
-            {
-                data: 'birthDate',
-                width: '12%',
-                render: function (data) {
-                    return moment(data).format('DD/MM/YYYY')
-                },
-            },
-            { data: 'gender', width: '8%' },
-            {
-                data: 'id',
-                render: function (data) {
-                    return `
+      },
+    ],
+    language: {
+      url: activeLanguagePath,
+    },
+    initComplete: function () {
+      var btns = $('.dt-button, .buttons-pdf, .buttons-html5')
+      btns.removeClass()
+      btns.addClass('btn btn-success')
+      btns.attr('id', 'ExportBtn')
+
+      $('#ExportBtn').prependTo($('#outside'))
+    },
+    ajax: { url: '/visitor/getall' },
+    columns: [
+      { data: 'name', width: '30%' },
+      { data: 'surname', width: '30%' },
+      {
+        data: 'birthDate',
+        width: '12%',
+        render: function (data) {
+          return moment(data).format('DD/MM/YYYY')
+        },
+      },
+      { data: 'gender', width: '8%' },
+      {
+        data: 'id',
+        render: function (data) {
+          return `
                             <div class="d-flex justify-content-around align-items-center">
                                <a onclick="showInPopup('/visitor/addoredit/${data}', 
                                'Update Visitor')" class="btn btn-warning"> ${editBtnText}</a>
@@ -93,120 +111,129 @@ function loadDataTable(exportBtnText, editBtnText, deleteBtnText, activeLanguage
                                </a>
                             </div>
                            `
-                },
-                width: '20%',
-            },
-        ],
-        columnDefs: [{
-            'targets': [4], 
-            'orderable': false
-        }]
-    });
+        },
+        width: '20%',
+      },
+    ],
+    columnDefs: [
+      {
+        targets: [4],
+        orderable: false,
+      },
+    ],
+  })
 }
 
 showInPopup = (url, title) => {
-    $.ajax({
-        type: 'GET',
-        url: url,
-        success: function (res) {
-            $('#form-modal .modal-body').html(res)
-            $('#form-modal .modal-title').html(title)
-            $('#form-modal').modal('show')
+  $.ajax({
+    type: 'GET',
+    url: url,
+    success: function (res) {
+      $('#form-modal .modal-body').html(res)
+      $('#form-modal .modal-title').html(title)
+      $('#form-modal').modal('show')
 
-            if ($('#citylist option:selected').val() == 0) {
-                $('#statelist').hide();
-            }
-            else {
-                $('#statelist').show();
-            }
-        },
-    })
+      if ($('#citylist option:selected').val() == 0) {
+        $('#statelist').hide()
+      } else {
+        $('#statelist').show()
+      }
+    },
+  })
 }
 
 function fillStateList(cityId, selectedIndex) {
-    $.ajax({
-        type: 'GET',
-        url: '/state/getallbycity?cityId=' + cityId,
-        success: function (res) {
+  $.ajax({
+    type: 'GET',
+    url: '/state/getallbycity?cityId=' + cityId,
+    success: function (res) {
+      $('#Visitor_VisitorAddress_StateId').empty()
 
-            $('#Visitor_VisitorAddress_StateId').slice(1).empty();
+      var stateSelectionText = document.getElementById(
+        'AddOrEditModalStateSelectionText'
+      ).value
 
-            $.each(res.data, function (index, value) {
-                $('#Visitor_VisitorAddress_StateId').append(
-                    $('<option value="' + value.id + '">' + value.name + '</option>'))
-            }
-            );
+      $('#Visitor_VisitorAddress_StateId').append(
+        $('<option disabled value="0">' + stateSelectionText + '</option>')
+      )
 
-            $('#Visitor_VisitorAddress_StateId option:eq(' + selectedIndex + ')').prop('selected', true);
-        },
-        error: function (err) {
-            console.log(err)
-        }
-    })
+      $.each(res.data, function (index, value) {
+        $('#Visitor_VisitorAddress_StateId').append(
+          $('<option value="' + value.id + '">' + value.name + '</option>')
+        )
+      })
+
+      $(
+        '#Visitor_VisitorAddress_StateId option:eq(' + selectedIndex + ')'
+      ).prop('selected', true)
+    },
+    error: function (err) {
+      console.log(err)
+    },
+  })
 }
 
 AddRecord = (form) => {
-    try {
-        $.ajax({
-            type: 'POST',
-            url: form.action,
-            data: new FormData(form),
-            contentType: false,
-            processData: false,
-            success: function (res) {
-                if (res.isValid) {
-                    $('#form-modal .modal-body').html('')
-                    $('#form-modal .modal-title').html('')
-                    $('#form-modal').modal('hide')
+  try {
+    $.ajax({
+      type: 'POST',
+      url: form.action,
+      data: new FormData(form),
+      contentType: false,
+      processData: false,
+      success: function (res) {
+        if (res.isValid) {
+          $('#form-modal .modal-body').html('')
+          $('#form-modal .modal-title').html('')
+          $('#form-modal').modal('hide')
 
-                    dataTable.ajax.reload()
-                } else {
-                    $('#form-modal .modal-body').html(res.html)
-                }
-            },
-            error: function (err) {
-                console.log(err)
-            },
-        })
-    } catch (e) {
-        console.log(e)
-    }
+          dataTable.ajax.reload()
+        } else {
+          $('#form-modal .modal-body').html(res.html)
+        }
+      },
+      error: function (err) {
+        console.log(err)
+      },
+    })
+  } catch (e) {
+    console.log(e)
+  }
 
-    return false
+  return false
 }
 
 function deleteRecord(url) {
-    if (confirm('Are you sure to delete this record?')) {
-        try {
-            $.ajax({
-                type: 'DELETE',
-                url: url,
-                success: function (res) {
-                    alert(res.entityValue + ' Deleted Successfully')
+  if (confirm('Are you sure to delete this record?')) {
+    try {
+      $.ajax({
+        type: 'DELETE',
+        url: url,
+        success: function (res) {
+          alert(res.entityValue + ' Deleted Successfully')
 
-                    dataTable.ajax.reload()
-                },
-                error: function (err) {
-                    alert('An Error Occurred While Deleting')
+          dataTable.ajax.reload()
+        },
+        error: function (err) {
+          alert('An Error Occurred While Deleting')
 
-                    console.log(err)
-                },
-            })
-        } catch (e) { }
-    }
+          console.log(err)
+        },
+      })
+    } catch (e) {}
+  }
 
-    return false
+  return false
 }
 
 function cityChange() {
-    if ($('#citylist option:selected').val() == 0) {
-        $('#statelist').hide();
-    }
-    else {
-        $('#statelist').show();
+  if ($('#citylist option:selected').val() == 0) {
+    $('#statelist').hide()
+  } else {
+    $('#statelist').show()
 
-        var cityId = $('#citylist option:selected').val();
+    var cityId = $('#citylist option:selected').val()
 
-        fillStateList(cityId, 0);
-    }
+    fillStateList(cityId, 0)
+  }
 }
