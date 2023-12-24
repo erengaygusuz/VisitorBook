9c20209ae215eb10ac7955db1fadbb6e82e805f8
Eren Gaygusuz <gaygusuzeren@gmail.com>
2023-12-24 14:04:09 +0300

    Users and Profiles Controller are Added

---
+++ VisitorBook.Backend/VisitorBook.Backend.Core/Dtos/ProfileDtos/PasswordChangeRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace VisitorBook.Backend.Core.Dtos.ProfileDtos
+{
+    public class PasswordChangeRequestDto
+    {
+        public string PasswordOld { get; set; }
+
+        public string PasswordNew { get; set; }
+
+        public string PasswordNewConfirm { get; set; }
+    }
+}
