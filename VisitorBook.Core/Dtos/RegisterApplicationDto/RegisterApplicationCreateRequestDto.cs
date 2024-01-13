ad06ad4bf8858c52583919a452c0a77892240edf
Eren Gaygusuz <gaygusuzeren@gmail.com>
2024-01-13 19:40:44 +0300

    Client Side Validation Rules are Added for RegisterApplication

--- VisitorBook.Core/Dtos/RegisterApplicationDto/RegisterApplicationCreateRequestDto.cs
+++ VisitorBook.Core/Dtos/RegisterApplicationDto/RegisterApplicationCreateRequestDto.cs
@@ -1,17 +1,34 @@
-﻿namespace VisitorBook.Core.Dtos.RegisterApplicationDto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VisitorBook.Core.Dtos.RegisterApplicationDto
 {
     public class RegisterApplicationCreateRequestDto
     {
         public int Id { get; set; }
 
+        [Required]
+        [MinLength(3)]
+        [MaxLength(100)]
+        [RegularExpression("^((?![0-9]).)*$")]
         public string Name { get; set; }
 
+        [Required]
+        [MinLength(3)]
+        [MaxLength(100)]
+        [RegularExpression("^((?![0-9]).)*$")]
         public string Surname { get; set; }
 
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
 
+        [Required]
+        [RegularExpression("^((?![0-9]).)*$")]
         public string Username { get; set; }
 
+        [Required]
+        [MinLength(20)]
+        [MaxLength(500)]
         public string Explanation { get; set; }
 
         public string Status { get; set; }
