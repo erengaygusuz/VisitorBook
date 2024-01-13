4bd1e9b15164c15bbc07b870bb17564e5beb92e0
Eren Gaygusuz <gaygusuzeren@gmail.com>
2024-01-13 11:12:41 +0300

    Client Side Validation Rules are Added for Contact Message

--- VisitorBook.Core/Dtos/ContactMessageDtos/ContactMessageRequestDto.cs
+++ VisitorBook.Core/Dtos/ContactMessageDtos/ContactMessageRequestDto.cs
@@ -1,10 +1,27 @@
-﻿namespace VisitorBook.Core.Dtos.ContactMessageDtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VisitorBook.Core.Dtos.ContactMessageDtos
 {
     public class ContactMessageRequestDto
     {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(100)]
+        [RegularExpression("^((?![0-9]).)*$")]
         public string NameSurname { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(150)]
         public string Subject { get; set; }
+
+        [Required]
+        [MinLength(20)]
+        [MaxLength(500)]
         public string Message { get; set; }
     }
 }
