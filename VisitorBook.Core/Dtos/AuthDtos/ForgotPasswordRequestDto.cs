60cb4d8031ff3d22c4af856070d41485b4cad3eb
Eren Gaygusuz <gaygusuzeren@gmail.com>
2024-01-13 20:12:49 +0300

    Client Side Validation Rules are Added for Forgot Password

--- VisitorBook.Core/Dtos/AuthDtos/ForgotPasswordRequestDto.cs
+++ VisitorBook.Core/Dtos/AuthDtos/ForgotPasswordRequestDto.cs
@@ -1,7 +1,11 @@
-﻿namespace VisitorBook.Core.Dtos.AuthDtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VisitorBook.Core.Dtos.AuthDtos
 {
     public class ForgotPasswordRequestDto 
     {
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
     }
 }
