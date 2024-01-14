a181c811cbc0d0c01ea8071d39d320f76d61344a
Eren Gaygusuz <gaygusuzeren@gmail.com>
2024-01-14 09:35:10 +0300

    Client Side Validation Rules are Added for Fake Data

--- VisitorBook.Core/ViewModels/FakeDataViewModel.cs
+++ VisitorBook.Core/ViewModels/FakeDataViewModel.cs
@@ -1,8 +1,15 @@
-﻿namespace VisitorBook.Core.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VisitorBook.Core.ViewModels
 {
     public class FakeDataViewModel
     {
+        [Required]
+        [Range(1, 99)]
         public int UserAmount { get; set; }
+
+        [Required]
+        [Range(1, 99)]
         public int VisitedCountyAmount { get; set; }
     }
 }
